234ffce5bc73e49df86992cc4cb6c629badb38e2
alejandro123456Y <mercedreyesa@gmail.com>
2026-05-28 02:33:42 -0600

    Configurar JWT URLs

--- config/urls.py
+++ config/urls.py
@@ -7,10 +7,23 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
 
-    path('api/login/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
-
+    # API EVENTOS
     path('api/', include('eventos.urls')),
+
+    # JWT LOGIN
+    path(
+        'api/login/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+
+    # REFRESH TOKEN
+    path(
+        'api/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
 ]
